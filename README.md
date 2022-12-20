64c4789207b80400ada65ac3847347bb2041d210
Black-crypto-gif <80727888+Black-crypto-gif@users.noreply.github.com>
2022-12-20 02:37:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# How-To-Fetch-Data-Using-React.js-Simple-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-eidaz8)
---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+
+import './App.css';
+
+function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        console.error('Fetching Error :', error);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return 'Loading...';
+  if (error)
+    return 'Error while fetching data check the link endpoint or your code ';
+  return;
+  <div className="App">
+    <h1>heelo</h1>
+  </div>;
+}
+
+export default App;
